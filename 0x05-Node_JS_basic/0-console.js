3fba4800c21e6b742b9124823cb1075cce81afaa
Austine <90908172+aaustinoduor@users.noreply.github.com>
2024-07-23 13:44:24 +0300

    Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * logs message to console.
+ * @param {string} message - the message to log.
+ * @returns {void}
+ */
+// arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * exports displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// default module export.
+module.exports = displayMessage;
